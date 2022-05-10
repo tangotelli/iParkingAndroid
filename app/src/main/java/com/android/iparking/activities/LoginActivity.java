b2bb3ea0f12200d3a6efbd227ab7bdadafcf48d5
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2022-05-10 18:50:48 +0200

    map activity shows device location

--- app/src/main/java/com/android/iparking/activities/LoginActivity.java
+++ app/src/main/java/com/android/iparking/activities/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.android.iparking.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -24,12 +30,14 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class LoginActivity extends AppCompatActivity {
-
-    private static final String API_BASE_URL = "https://192.168.1.141:8000";
+public class LoginActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     private APIService apiService;
     private User user;
+
+    private static final String API_BASE_URL = "https://192.168.1.141:8000";
+    private static final int LOCATION_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,12 +87,16 @@ public class LoginActivity extends AppCompatActivity {
 
     private void completeLogin(UserPojo userPojo) {
         this.user = User.fromPojo(userPojo);
-        //lanzar actividad del mapa
-        Snackbar.make(
-                findViewById(android.R.id.content),
-                "Todo bien",
-                Snackbar.LENGTH_LONG
-        ).show();
+        if (ContextCompat
+                .checkSelfPermission(LoginActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat
+                    .requestPermissions(LoginActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            LOCATION_PERMISSION_CODE);
+        } else {
+            this.showMap();
+        }
     }
 
     private void incompleteLogin(int code) {
@@ -94,6 +106,29 @@ public class LoginActivity extends AppCompatActivity {
                 getString(R.string.auth_failure),
                 Snackbar.LENGTH_LONG
         ).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                this.showMap();
+            } else {
+                Snackbar.make(
+                        findViewById(android.R.id.content),
+                        getString(R.string.permission_denied),
+                        Snackbar.LENGTH_LONG
+                ).show();
+            }
+        }
+    }
+
+    private void showMap() {
+        Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+        intent.putExtra("user", this.user);
+        startActivity(intent);
     }
 
     public void signin(View view) {
--- app/src/main/java/com/android/iparking/activities/MapActivity.java
+++ app/src/main/java/com/android/iparking/activities/MapActivity.java
@@ -1,16 +1,96 @@
 package com.android.iparking.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 
 import com.android.iparking.R;
+import com.android.iparking.models.User;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private Location deviceLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private User user;
+
+    private static final int LOCATION_PERMISSION_CODE = 100;
+    private static final float STREETS_ZOOM_LEVEL = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        this.getLocationUpdates();
+        this.user = (User) getIntent().getSerializableExtra("user");
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.map = googleMap;
+        if (ContextCompat
+                .checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            this.map.setMyLocationEnabled(true);
+        }
+        if (deviceLocation != null) {
+            LatLng current = new LatLng(deviceLocation.getLatitude(), deviceLocation.getLongitude());
+            map.moveCamera(CameraUpdateFactory.newLatLng(current));
+            map.moveCamera(CameraUpdateFactory.zoomTo(STREETS_ZOOM_LEVEL));
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocationUpdates() {
+        this.fusedLocationClient = LocationServices
+                .getFusedLocationProviderClient(this);
+        this.locationRequest = LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        this.locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        deviceLocation = location;
+                    }
+                }
+            }
+        };
+        if (ContextCompat
+                .checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            this.fusedLocationClient.requestLocationUpdates(this.locationRequest,
+                    this.locationCallback, Looper.getMainLooper());
+        }
     }
 }
