518dc9d4d86fd97667cb038513365fc5013d9dbb
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2022-05-31 14:02:49 +0200

    fixing some security issues

--- app/src/main/java/com/android/iparking/connectivity/RetrofitFactory.java
+++ app/src/main/java/com/android/iparking/connectivity/RetrofitFactory.java
@@ -1,11 +1,13 @@
 package com.android.iparking.connectivity;
 
+import static com.android.iparking.BuildConfig.IPARKING_BASE_URL;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitFactory {
 
-    private static final String API_BASE_URL = "https://192.168.1.34:8000";
+    private static final String API_BASE_URL = IPARKING_BASE_URL;
 
     private RetrofitFactory() {
         //empty for framework
